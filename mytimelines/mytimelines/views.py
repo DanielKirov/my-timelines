d03a99276a663fea583a7f6ae31135e3e5d5fe69
Daniel Kirov <kirov.daniel@gmail.com>
2016-02-20 17:53:36 +0000

    Add listview for timelines.

--- mytimelines/mytimelines/views.py
+++ mytimelines/mytimelines/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
-
+from django.views.generic.list import ListView
 
 from .models import Timeline
+
+@login_required
+class TimelineListView(ListView):
+
+    model = Timeline
+    template_name = "timeline/list.html"
+
+    def get_queryset(self):
+        return Timeline.objects.filter(user=self.request.user)
+
 
 @login_required
 def timelineview(request, slug):
