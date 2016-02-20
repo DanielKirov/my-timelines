b7dc07563cfbf9d8a804e983448cb0d06e4a59a3
sparksmith <lord.sparksmith@gmail.com>
2016-02-20 13:45:24 +0000

    Change for the thumbnel creator to take an image + name to creat the name_thumbnail.jpg we use in timeline.

--- mytimelines/mytimelines/utilities/picofyer.py
+++ mytimelines/mytimelines/utilities/picofyer.py
@@ -1,14 +1,15 @@
 from PIL import Image, ImageOps, ImageDraw, ImageFilter
 
+
+
 size = (128, 128)
 
 
-def thumblyfy(imagename):
-    mask = Image.new('L', size, 0)
-    draw = ImageDraw.Draw(mask)
-    draw.ellipse((0, 0) + size, fill=255)
-    im = Image.open(imagename)
-    output = ImageOps.fit(im, mask.size, centering=(0.5, 0.5))
-    output.putalpha(mask)
-    output = output.filter(ImageFilter.SHARPEN)
-    output.save(imagename + '_thumbnail.png')
+def thumblyFy(im, name):
+	mask = Image.new('L', size, 0)
+	draw = ImageDraw.Draw(mask) 
+	draw.ellipse((0, 0) + size, fill=255)
+	output = ImageOps.fit(im, mask.size, centering=(0.5, 0.5))
+	output.putalpha(mask)
+	output = output.filter(ImageFilter.SHARPEN)
+	output.save(name+'_thumbnail.png')
