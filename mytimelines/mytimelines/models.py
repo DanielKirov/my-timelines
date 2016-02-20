09460c4e036ed180a6bdeed14a7e9c528533f895
Daniel Kirov <kirov.daniel@gmail.com>
2016-02-20 17:10:34 +0000

    Add event and event images models.

--- mytimelines/mytimelines/models.py
+++ mytimelines/mytimelines/models.py
@@ -8,3 +8,19 @@ class Timeline(models.Model):
     icon = models.ImageField(upload_to="icons")
     color = models.CharField(max_length=20)
 
+
+
+class Event(models.Model):
+
+    timeline = models.ForeignKey(Timeline, related_name="events")
+    title = models.CharField(max_length=200, null=False, blank=False)
+    date = models.DateField()
+    main_image = models.ImageField(upload_to="events")
+
+
+
+class EventPicture(models.Model):
+
+    event = models.ForeignKey(Event)
+    image = models.ImageField(upload_to="events")
+
