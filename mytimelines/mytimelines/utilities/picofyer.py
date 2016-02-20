aa647d3124c78e4dd85b571c959f50fdd0d0fac5
sparksmith <lord.sparksmith@gmail.com>
2016-02-20 13:54:33 +0000

    Thumbnail namer

--- mytimelines/mytimelines/utilities/picofyer.py
+++ mytimelines/mytimelines/utilities/picofyer.py
@@ -1,15 +1,19 @@
 from PIL import Image, ImageOps, ImageDraw, ImageFilter
 
-
-
 size = (128, 128)
 
-
-def thumblyFy(im, name):
+#take a name like pat.jpg.zip removes everything after first .
+# then generates a string pat
+def createName(imagename):
+        return imagename.split('.',1)[0]
+# take a Image.open(X) type of apraameter as an image and a name
+# generates a thumbnail 128x128 based on global var
+# named name_thumbnail.png
+def thumblyFy(im, imagename):
 	mask = Image.new('L', size, 0)
 	draw = ImageDraw.Draw(mask) 
 	draw.ellipse((0, 0) + size, fill=255)
 	output = ImageOps.fit(im, mask.size, centering=(0.5, 0.5))
 	output.putalpha(mask)
 	output = output.filter(ImageFilter.SHARPEN)
-	output.save(name+'_thumbnail.png')
+	output.save(createName(imagename)+'_thumbnail.png')
