ba95f63ae02d60d3c997bac94cab247b55dfd80e
MrJew <kouzmov@gmail.com>
2016-02-20 13:59:36 +0000

    adding color to model

--- mytimelines/mytimelines/models.py
+++ mytimelines/mytimelines/models.py
@@ -6,5 +6,5 @@ class Timeline(models.Model):
     user = models.ForeignKey(User, related_name="users")
     title = models.CharField(max_length=200)
     icon = models.ImageField(upload_to="icons")
-
+    color = models.CharField(max_length=20)
 
