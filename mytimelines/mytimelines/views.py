cb47afe139a636810bfa23435e6afa48262a6ae1
MrJew <kouzmov@gmail.com>
2016-02-20 17:53:17 +0000

    remove debugger

--- mytimelines/mytimelines/views.py
+++ mytimelines/mytimelines/views.py
@@ -31,6 +31,5 @@ def addTimeline(request):
         name = request.POST['name']
         color = request.POST['color']
         user = request.user
-        import pdb; pdb.set_trace()
         Timeline.objects.create(user=user,title=name,color=color,icon=file)
     return render(request, 'test.html')
