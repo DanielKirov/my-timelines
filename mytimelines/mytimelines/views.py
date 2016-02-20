d3cecafeb691e1b4aeb04b369dd321b881dd6f7f
MrJew <kouzmov@gmail.com>
2016-02-20 17:43:08 +0000

    add flat ui properly

--- mytimelines/mytimelines/views.py
+++ mytimelines/mytimelines/views.py
@@ -24,3 +24,13 @@ def homeview(request):
 def testview(request):
     #import pdb; pdb.set_trace()
     return render(request, 'test.html')
+
+def addTimeline(request):
+    if request.method == 'POST':
+        file = request.FILES['pic']
+        name = request.POST['name']
+        color = request.POST['color']
+        user = request.user
+        import pdb; pdb.set_trace()
+        Timeline.objects.create(user=user,title=name,color=color,icon=file)
+    return render(request, 'test.html')
