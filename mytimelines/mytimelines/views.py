99931764130d68c5452ce31de13ba839df3e8f0e
MrJew <kouzmov@gmail.com>
2016-02-20 17:31:53 +0000

    add proppert view

--- mytimelines/mytimelines/views.py
+++ mytimelines/mytimelines/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
-
+from .models import *
 
 
 def homeview(request):
@@ -11,4 +11,14 @@ def homeview(request):
 
 def testview(request):
     #import pdb; pdb.set_trace()
+    return render(request, 'test.html')
+
+def addTimeline(request):
+    if request.method == 'POST':
+        file = request.FILES['pic']
+        name = request.POST['name']
+        color = request.POST['color']
+        user = request.user
+        import pdb; pdb.set_trace()
+        Timeline.objects.create(user=user,title=name,color=color,icon=file)
     return render(request, 'test.html')
