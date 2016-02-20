4b091ad70230c3878dd559006880fb389ac19e4b
sparksmith <lord.sparksmith@gmail.com>
2016-02-20 13:20:39 +0000

    Quikc refactor of styling of python code.

--- mytimelines/mytimelines/utilities/picofyer.py
+++ mytimelines/mytimelines/utilities/picofyer.py
@@ -1,16 +1,14 @@
 from PIL import Image, ImageOps, ImageDraw, ImageFilter
 
-
-
 size = (128, 128)
 
 
-def thumblyFy(imageName):
-	mask = Image.new('L', size, 0)
-	draw = ImageDraw.Draw(mask) 
-	draw.ellipse((0, 0) + size, fill=255)
-	im = Image.open(imageName)
-	output = ImageOps.fit(im, mask.size, centering=(0.5, 0.5))
-	output.putalpha(mask)
-	output = output.filter(ImageFilter.SHARPEN)
-	output.save(imageName+'_thumbnail.png')
+def thumblyfy(imagename):
+    mask = Image.new('L', size, 0)
+    draw = ImageDraw.Draw(mask)
+    draw.ellipse((0, 0) + size, fill=255)
+    im = Image.open(imagename)
+    output = ImageOps.fit(im, mask.size, centering=(0.5, 0.5))
+    output.putalpha(mask)
+    output = output.filter(ImageFilter.SHARPEN)
+    output.save(imagename + '_thumbnail.png')
